f62a606acecf3eaf67193ca634caa1ea12eeb7f1
Kowsar Rahman (s3825159) <79802682+KowsarRahman@users.noreply.github.com>
2021-05-16 11:28:00 +0600

    Added some core Tile methods

--- Tile.cpp
+++ Tile.cpp
@@ -1,33 +1,71 @@
-
 #include "Tile.h"
 
-
-Tile::Tile(Colour colour, Shape shape) {
-	this->colour = colour;
-	this->shape = shape;
+Tile::Tile(Colour c, Shape s) {
+  this->colour = c;
+  this->shape = s;
 }
 
-void Tile::setColour(Colour colour) {
-	this->colour = colour;
+Tile::Tile(const Tile &t) {
+  colour = t.colour;
+  shape = t.shape;
+}
+
+Tile::Tile() {
+  shape = 0;
+  colour = ' ';
+}
+
+
+//Getters
+Shape Tile::getShape() {
+  return shape;
 }
 
 Colour Tile::getColour() {
-	return colour;
+  return colour;
 }
 
-void Tile::setShape(Shape shape) {
-	this->shape = shape;
+
+//Setters
+void Tile::setShape(Shape s) {
+  shape = s;
 }
 
-Shape Tile::getShape() {
-	return Shape;
+void Tile::setColour(Colour c) {
+  colour = c;
 }
 
-Tile::compareTile(Tile tileA, Tile tileB) {
-	if (TileA->getColour() == tileB.getColour() || TileA->getShape() && TileB->getShape()) {
-		compareResult = true;
-	} else {
-		compareResult = false;
-	}
-	return compareResult;
+//Tile Functions
+
+bool Tile::compareTile(Tile *tile) {
+  bool result = false;
+  if (this->colour == tile->getColour() && this->shape == tile->getShape()) {
+    result = true;
+  }
+  return result;
 }
+
+bool Tile::isSameColour(const Tile &tile) {
+  return colour == tile.colour;
+}
+
+bool Tile::isSameShape(const Tile &tile) {
+  return shape == tile.shape;
+}
+
+// std::ostream &operator<<(std::ostream &out, const Tile &tile) {
+//   out << tile.colour << std::to_string(tile.shape);
+//   return out;
+// }
+
+// Tile &Tile::operator=(const Tile &tile) {
+//   colour = tile.colour;
+//   shape = tile.shape;
+//   return *this;
+// }
+
+// bool operator==(const Tile &t1, const Tile &t2) {
+//   return t1.colour == t2.colour && t1.shape == t2.shape;
+// }
+
+
